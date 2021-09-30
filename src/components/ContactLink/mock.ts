1e0bb2a30cb5c0cef0daf894776f3b83cf8831a8
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-09-30 13:59:32 -0300

    feat: add ContactLink component

---
+++ src/components/ContactLink/mock.ts
@@ -0,0 +1,19 @@
+import { ContactLinkProps } from '.'
+
+export default [
+  {
+    name: 'Github',
+    icon: '/img/icons/contact/github.svg',
+    link: 'https://github.com/d3vlopes',
+  },
+  {
+    name: 'Linkedin',
+    icon: '/img/icons/contact/linkedin.svg',
+    link: 'https://www.linkedin.com/in/d3vlopes/',
+  },
+  {
+    name: 'Whatsapp',
+    icon: '/img/icons/contact/whats.svg',
+    link: 'https://api.whatsapp.com/send?phone=5551997465898&text=Contato%20pelo%20site',
+  },
+] as ContactLinkProps[]
---
+++ src/components/ContactLink/stories.tsx
@@ -0,0 +1,29 @@
+import { Story, Meta } from '@storybook/react'
+
+import ContactLink, { ContactLinkProps } from '.'
+import mock from './mock'
+
+export default {
+  title: 'Components/ContactLinks',
+  component: ContactLink,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/KIQWZ47GDrrNCIxysbEqOA/My-Portfolio?node-id=855%3A1062',
+    },
+  },
+  args: mock[0],
+  argTypes: {
+    icon: {
+      type: '',
+    },
+  },
+} as Meta<ContactLinkProps>
+
+export const Default: Story<ContactLinkProps> = (args) => {
+  return (
+    <div style={{ display: 'flex', gap: '1.6rem' }}>
+      <ContactLink {...args} />
+    </div>
+  )
+}
