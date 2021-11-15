049f27d4db871b9020c1cb40a06eb73041fe750d
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-15 13:34:10 -0300

    fix: correcting source API routes

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/api/contact',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
