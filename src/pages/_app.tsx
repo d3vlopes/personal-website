51fa0d260569b12733096f0717acba46b70eacbb
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-15 20:39:52 -0300

    feat: config graphQL client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,26 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
-import { ApolloProvider } from '@apollo/client'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/themes/default'
 
-import { useApollo } from 'utils/apollo'
-
 function App({ Component, pageProps }: AppProps) {
-  const client = useApollo(pageProps.initialApolloState)
-
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <title>Leandro Lopes - Desenvolvedor Frontend | UI Design</title>
-          <link rel="shortcut icon" href="/img/icon-512.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta
-            name="description"
-            content="Site para mostrar sobre mim e meus trabalhos como desenvolvedor e design"
-          />
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ApolloProvider>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Leandro Lopes - Desenvolvedor Frontend | UI Design</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="Site para mostrar sobre mim e meus trabalhos como desenvolvedor e design"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
