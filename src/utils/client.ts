978ae9138ef6e5d782da751068ee2ff8248edb9e
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-16 14:27:18 -0300

    feat: add ContactLinks fragment

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -2,10 +2,8 @@ import { GraphQLClient } from 'graphql-request'
 
 const endpoint = process.env.GRAPHQL_HOST || ''
 
-const client = new GraphQLClient(endpoint, {
+export const client = new GraphQLClient(endpoint, {
   headers: {
     authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`,
   },
 })
-
-export default client
