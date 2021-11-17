7c098107e90022c938fda00f29be292e41e6e0ca
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-17 15:04:01 -0300

    feat: add util to map API data

---
+++ src/utils/mappers/index.ts
@@ -0,0 +1,60 @@
+import {
+  ContactLinksProps,
+  MenuLinkProps,
+  ProjectsRecentsProps,
+  SkillsProps,
+  ToolsProps,
+} from 'types/mappers'
+
+export const linksMapper = (links: MenuLinkProps[]) => {
+  return links.map((link) => ({
+    id: link.id,
+    path: link.path,
+    text: link.menuLabel,
+  }))
+}
+
+export const contactsLinksMapper = (contactsLinks: ContactLinksProps[]) => {
+  return contactsLinks.map((contactLink) => ({
+    name: contactLink.name,
+    icon: contactLink.icon.url,
+    link: contactLink.link,
+  }))
+}
+
+export const skillsMapper = (skills: SkillsProps[]) => {
+  return skills.map((skill) => ({
+    icon: skill.icon.url,
+    title: skill.title,
+    description: skill.description,
+  }))
+}
+
+export const toolsMapper = (tools: ToolsProps[]) => {
+  const developerTools = tools.filter((tool) => tool.type === 'developer')
+  const designTools = tools.filter((tool) => tool.type === 'design')
+
+  const developer = developerTools.map((tool: ToolsProps) => ({
+    src: tool.src.url,
+    name: tool.name,
+  }))
+
+  const design = designTools.map((tool: ToolsProps) => ({
+    src: tool.src.url,
+    name: tool.name,
+  }))
+
+  return {
+    developer,
+    design,
+  }
+}
+
+export const projectsRecentsMapper = (projects: ProjectsRecentsProps[]) => {
+  return projects.map((project) => ({
+    src: project.thumbnail?.url,
+    name: project.name,
+    description: project.description,
+    slug: project.slug,
+  }))
+}
---
+++ src/utils/mappers/test.ts
@@ -0,0 +1,186 @@
+import {
+  linksMapper,
+  contactsLinksMapper,
+  skillsMapper,
+  toolsMapper,
+  projectsRecentsMapper,
+} from '.'
+import {
+  ContactLinksProps,
+  MenuLinkProps,
+  SkillsProps,
+  ToolsProps,
+  ProjectsRecentsProps,
+} from 'types/mappers'
+
+describe('linksMapper', () => {
+  it('should return the right format when mapped', () => {
+    const links = [
+      {
+        id: '1',
+        path: '/',
+        menuLabel: 'Home',
+      },
+      {
+        id: '2',
+        path: '/projects',
+        menuLabel: 'Projetos',
+      },
+    ] as MenuLinkProps[]
+
+    expect(linksMapper(links)).toStrictEqual([
+      {
+        id: '1',
+        path: '/',
+        text: 'Home',
+      },
+      {
+        id: '2',
+        path: '/projects',
+        text: 'Projetos',
+      },
+    ])
+  })
+})
+
+describe('contactLinksMapper', () => {
+  it('should return the right format when mapped', () => {
+    const contactLinks = [
+      {
+        name: 'Github',
+        icon: {
+          url: 'icon-url-github',
+        },
+        link: 'https://github.com',
+      },
+      {
+        name: 'Linkedin',
+        icon: {
+          url: 'icon-url-linkedin',
+        },
+        link: 'https://linkedin.com',
+      },
+    ] as ContactLinksProps[]
+
+    expect(contactsLinksMapper(contactLinks)).toStrictEqual([
+      {
+        name: 'Github',
+        icon: 'icon-url-github',
+        link: 'https://github.com',
+      },
+      {
+        name: 'Linkedin',
+        icon: 'icon-url-linkedin',
+        link: 'https://linkedin.com',
+      },
+    ])
+  })
+})
+
+describe('skillsMapper', () => {
+  it('should return the right format when mapped ', () => {
+    const skills = [
+      {
+        title: 'Desafios',
+        icon: {
+          url: 'icon-url-desafios',
+        },
+        description: 'Loren ipsum dolor',
+      },
+      {
+        title: 'Persistência',
+        icon: {
+          url: 'icon-url-persistencia',
+        },
+        description: 'Loren ipsum dolor',
+      },
+    ] as SkillsProps[]
+
+    expect(skillsMapper(skills)).toStrictEqual([
+      {
+        title: 'Desafios',
+        icon: 'icon-url-desafios',
+        description: 'Loren ipsum dolor',
+      },
+      {
+        title: 'Persistência',
+        icon: 'icon-url-persistencia',
+        description: 'Loren ipsum dolor',
+      },
+    ])
+  })
+})
+
+describe('toolsMapper', () => {
+  it('should return the right format when mapped', () => {
+    const tools = [
+      {
+        type: 'developer',
+        name: 'React',
+        src: {
+          url: 'icon-url-react',
+        },
+      },
+      {
+        type: 'design',
+        name: 'Figma',
+        src: {
+          url: 'icon-url-figma',
+        },
+      },
+    ] as ToolsProps[]
+
+    expect(toolsMapper(tools)).toStrictEqual({
+      developer: [
+        {
+          name: 'React',
+          src: 'icon-url-react',
+        },
+      ],
+      design: [
+        {
+          name: 'Figma',
+          src: 'icon-url-figma',
+        },
+      ],
+    })
+  })
+})
+
+describe('projectsRecentsMapper', () => {
+  it('should return the right format when mapped', () => {
+    const projects = [
+      {
+        thumbnail: {
+          url: 'thumbnail-url-project-1',
+        },
+        name: 'Project 1',
+        description: 'Loren ipsum dolor',
+        slug: 'project-1',
+      },
+      {
+        thumbnail: {
+          url: 'thumbnail-url-project-2',
+        },
+        name: 'Project 2',
+        description: 'Loren ipsum dolor',
+        slug: 'project-2',
+      },
+    ] as ProjectsRecentsProps[]
+
+    expect(projectsRecentsMapper(projects)).toStrictEqual([
+      {
+        src: 'thumbnail-url-project-1',
+        name: 'Project 1',
+        description: 'Loren ipsum dolor',
+        slug: 'project-1',
+      },
+      {
+        src: 'thumbnail-url-project-2',
+        name: 'Project 2',
+        description: 'Loren ipsum dolor',
+        slug: 'project-2',
+      },
+    ])
+  })
+})
