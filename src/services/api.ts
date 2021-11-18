efb4caa9303b2d939e31fae1b807055e4fa27b56
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-18 19:54:41 -0300

    feat: add api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from 'graphql-request'
 
 const endpoint = process.env.GRAPHQL_HOST || ''
 
-export const client = new GraphQLClient(endpoint, {
+export const api = new GraphQLClient(endpoint, {
   headers: {
     authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`,
   },
