a1923103f70d8c2889f84a15cc0c0feaf43add28
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-26 15:19:54 -0300

    feat: add next-seo config

---
+++ next-seo.config.js
@@ -0,0 +1,9 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt-BR',
+    url: 'https://my-portfolio-d3vlopes.vercel.app',
+    site_name: 'Leandro Lopes',
+    title: 'Leandro Lopes',
+  },
+}
