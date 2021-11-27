fd91f36c7e2b6afbd394dbf353df8b3ecf6b929c
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-11-26 22:28:31 -0300

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { DefaultSeo } from 'next-seo'
+import Script from 'next/script'
 
 import SEO from '../../next-seo.config'
 
@@ -21,8 +22,20 @@ function App({ Component, pageProps }: AppProps) {
           content="Site pessoal onde falo sobre mim e dos meus trabalhos como desenvolvedor e design"
         />
       </Head>
-      <DefaultSeo {...SEO} />
       <GlobalStyles />
+      <DefaultSeo {...SEO} />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="ga-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   )
